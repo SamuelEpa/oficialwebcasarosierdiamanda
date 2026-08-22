06b634edd9c8dfbbeb54ef69ad7142d5c764deff
samuel <samuel.dev2199@gmail.com>
2026-08-22 08:09:51 -0400

    wip:migracion footer and HomeHero

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,6 @@ import "./public-header-desktop.css";
 import "./admin-offerings-table.css";
 import "../components/home/gift-carousel.css";
 import "../components/home/social-gallery-home.css";
-import "../components/layout/footer/public-footer-editorial.css";
 import "../components/layout/scroll-nav/home-scroll-sticky-nav.css";
 
 const baskervville = Baskervville({
